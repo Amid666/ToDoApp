43eb4d9a987713c09528042e4534132ff6249f36
Amid666 <zuzukakabuba@gmail.com>
2023-04-15 12:49:59 +0300

    Added file DetailViewController

---
+++ ToDoApp/Controller/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  ToDoApp
+//
+//  Created by Кефир Кефирчик on 15.04.2023.
+//
+
+import UIKit
+import MapKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var titleLabel: UIView!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var dateLabel: UIView!
+    @IBOutlet var mapView: MKMapView!
+}
--- ToDoAppTests/Controller/DetailViewControllerTests.swift
+++ ToDoAppTests/Controller/DetailViewControllerTests.swift
@@ -9,28 +9,36 @@ import XCTest
 @testable import ToDoApp
 
 final class DetailViewControllerTests: XCTestCase {
+    
+    var sut: DetailViewController!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        sut = storyboard.instantiateViewController(withIdentifier: String(describing: DetailViewController.self)) as? DetailViewController
+        sut.loadViewIfNeeded()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testHasTitlelabel() {
+        XCTAssertNotNil(sut.titleLabel)
+        XCTAssertTrue(sut.titleLabel.isDescendant(of: sut.view))
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testHasDescriptionlabel() {
+        XCTAssertNotNil(sut.descriptionLabel)
+        XCTAssertTrue(sut.descriptionLabel.isDescendant(of: sut.view))
     }
-
+    
+    func testHasDatelabel() {
+        XCTAssertNotNil(sut.dateLabel)
+        XCTAssertTrue(sut.dateLabel.isDescendant(of: sut.view))
+    }
+    
+    func testHasMapView() {
+        XCTAssertNotNil(sut.mapView)
+        XCTAssertTrue(sut.mapView.isDescendant(of: sut.view))
+    }
 }
